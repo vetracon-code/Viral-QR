e7012ca8ece1a96b4eb2adcca14203980c6753eb
Vetracon <vetracon@gmail.com>
2026-05-07 10:18:23 +0200

    Fix heredoc e struttura admin-utente

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const cors = require('cors');
-const QRCode = require('qrcode');
+const path = require('path');
 
 const app = express();
-app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.static(__dirname));
 
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
-
-let globalEndTime = null;
+let gameState = {
+    endTime: null,
+    active: false,
+    users: {}
+};
 
 io.on('connection', (socket) => {
-    socket.emit('init', { globalEndTime });
-    socket.on('createMaster', (durationMinutes) => {
-        if (!globalEndTime) {
-            globalEndTime = Date.now() + (durationMinutes * 60000);
-            io.emit('timerStarted', globalEndTime);
-        }
+    socket.emit('sync', gameState);
+    socket.on('adminStart', (data) => {
+        const now = Date.now();
+        let durationMs = 0;
+        if (data.unit === 'min') durationMs = data.value * 60000;
+        else if (data.unit === 'ore') durationMs = data.value * 3600000;
+        else if (data.unit === 'giorni') durationMs = data.value * 86400000;
+        gameState.endTime = now + durationMs;
+        gameState.active = true;
+        io.emit('timerStarted', gameState.endTime);
+    });
+    socket.on('registerUser', (userData) => {
+        gameState.users[socket.id] = { nick: userData.nick, likes: 0 };
+        socket.emit('profileCreated', { id: socket.id, nick: userData.nick });
     });
 });
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => console.log('Server pronto'));
+server.listen(PORT, () => console.log('Server in ascolto'));
