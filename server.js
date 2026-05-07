403461a61132c3e0b7be4f91e3c78d28e875c79a
Vetracon <vetracon@gmail.com>
2026-05-07 10:47:38 +0200

    Aggiunta mappa Leaflet, inviti WA e passaporto QR

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const io = new Server(server);
 
 app.use(express.static(__dirname));
 
-const randomNicks = ["EcoFantasma", "StellaRandagia", "LampoNascosto", "VentoLibero", "NebbiaArgentea", "ScattoRapido", "OrizzonteBlu", "MisteroPuro"];
-
 let gameState = {
+    name: "Viral Zone Milano",
+    startTime: null,
     endTime: null,
     active: false,
+    center: { lat: 45.4642, lng: 9.1900 }, // Default: Milano
+    radius: 500, // metri
     users: {}
 };
 
@@ -20,44 +22,33 @@ io.on('connection', (socket) => {
     socket.emit('sync', gameState);
 
     socket.on('adminStart', (data) => {
-        let durationMs = 0;
-        if (data.unit === 'min') durationMs = data.value * 60000;
-        else if (data.unit === 'ore') durationMs = data.value * 3600000;
-        else if (data.unit === 'giorni') durationMs = data.value * 86400000;
+        gameState.name = data.name || "Evento Live";
+        let durationMs = data.unit === 'min' ? data.value * 60000 : data.value * 3600000;
         gameState.endTime = Date.now() + durationMs;
         gameState.active = true;
-        io.emit('timerStarted', gameState.endTime);
+        io.emit('timerStarted', { endTime: gameState.endTime, name: gameState.name });
     });
 
     socket.on('registerUser', (userData) => {
-        let finalNick = userData.nick.trim();
-        if (!finalNick) {
-            finalNick = randomNicks[Math.floor(Math.random() * randomNicks.length)] + Math.floor(Math.random() * 100);
-        }
-        
         gameState.users[socket.id] = { 
             id: socket.id,
-            nick: finalNick, 
-            reactions: { '👍': 0, '❤️': 0, '🔥': 0, '🙌': 0, '😎': 0, '✨': 0 } 
+            nick: userData.nick || "User" + Math.floor(Math.random()*100),
+            lat: userData.lat,
+            lng: userData.lng,
+            reactions: { '👍': 0, '❤️': 0, '🔥': 0, '🙌': 0, '😎': 0, '✨': 0 }
         };
         socket.emit('profileCreated', { id: socket.id, user: gameState.users[socket.id] });
+        io.emit('updateMap', gameState.users);
     });
 
-    socket.on('sendReaction', (data) => {
-        // data: { toId, icon, fromId, fromNick }
-        if (gameState.users[data.toId]) {
-            gameState.users[data.toId].reactions[data.icon]++;
-            // Invia la notifica specifica al destinatario per permettergli di ricambiare
-            io.to(data.toId).emit('receiveNotification', {
-                fromId: data.fromId,
-                fromNick: data.fromNick,
-                icon: data.icon
-            });
-            // Aggiorna i contatori globali per il destinatario
-            io.to(data.toId).emit('updateMyReactions', gameState.users[data.toId].reactions);
+    socket.on('updateLocation', (coords) => {
+        if (gameState.users[socket.id]) {
+            gameState.users[socket.id].lat = coords.lat;
+            gameState.users[socket.id].lng = coords.lng;
+            io.emit('updateMap', gameState.users);
         }
     });
 });
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => console.log('Server con Ricambia pronto'));
+server.listen(PORT, () => console.log('Server Mappa Pronto'));
