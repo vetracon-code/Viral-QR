151babcba6707e35d7622031970f0539c0026389
Vetracon <vetracon@gmail.com>
2026-05-07 10:28:11 +0200

    Sistema social con Like, contatori e Log interazioni

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,26 +11,43 @@ app.use(express.static(__dirname));
 let gameState = {
     endTime: null,
     active: false,
-    users: {}
+    users: {} // Struttura: { socketId: { nick, reactions: { '❤️': 0, '👍': 0, ... } } }
 };
 
 io.on('connection', (socket) => {
     socket.emit('sync', gameState);
+
     socket.on('adminStart', (data) => {
-        const now = Date.now();
         let durationMs = 0;
         if (data.unit === 'min') durationMs = data.value * 60000;
         else if (data.unit === 'ore') durationMs = data.value * 3600000;
         else if (data.unit === 'giorni') durationMs = data.value * 86400000;
-        gameState.endTime = now + durationMs;
+        gameState.endTime = Date.now() + durationMs;
         gameState.active = true;
         io.emit('timerStarted', gameState.endTime);
     });
+
     socket.on('registerUser', (userData) => {
-        gameState.users[socket.id] = { nick: userData.nick, likes: 0 };
-        socket.emit('profileCreated', { id: socket.id, nick: userData.nick });
+        gameState.users[socket.id] = { 
+            nick: userData.nick, 
+            reactions: { '👍': 0, '❤️': 0, '🔥': 0, '🙌': 0, '😎': 0, '✨': 0 } 
+        };
+        socket.emit('profileCreated', { id: socket.id, user: gameState.users[socket.id] });
+        io.emit('updateUsers', gameState.users);
+    });
+
+    socket.on('sendReaction', (data) => {
+        // data = { toId: 'socket_id', icon: '❤️', fromNick: 'Pippo' }
+        if (gameState.users[data.toId]) {
+            gameState.users[data.toId].reactions[data.icon]++;
+            io.emit('reactionUpdate', { 
+                toId: data.toId, 
+                reactions: gameState.users[data.toId].reactions,
+                log: `${data.fromNick} ha inviato ${data.icon}`
+            });
+        }
     });
 });
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => console.log('Server in ascolto'));
+server.listen(PORT, () => console.log('Server Social pronto'));
